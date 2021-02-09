a8e33ae727a7894c86f07b363e2f7befde67b10a
paksisoma <78373326+paksisoma@users.noreply.github.com>
2021-02-09 18:26:21 +0100

    Első feladat

---
+++ megfejtes/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace megfejtes
+{
+
+    class Program
+    {
+        static char[,] rejtveny;
+        static List<string> rejtvenyszo = new List<string>();
+
+        static void Main(string[] args)
+        {
+            const string eleresiut = @"rejtveny.txt";
+
+            Console.WriteLine("1. feladat:");
+            Elso(eleresiut);
+
+            Console.ReadKey();
+        }
+
+        static void Elso(string eleresiut)
+        {
+            StreamReader fajl = new StreamReader(eleresiut);
+            string sor = "";
+            int index = -1;
+
+            while ((sor = fajl.ReadLine()) != null)
+            {
+                if (index == -1)
+                {
+                    string[] temp = sor.Split(' ');
+
+                    rejtveny = new char[int.Parse(temp[0]), int.Parse(temp[1])];
+                }
+                else
+                {
+                    char[] temp = sor.ToCharArray();
+                    string[] temp2 = sor.Split('X');
+
+                    for (int i = 0; i < temp2.Length; i++)
+                    {
+                        if (temp2[i] != "" && temp2[i] != " ")
+                        {
+                            rejtvenyszo.Add(temp2[i]);
+                        }
+                    }
+
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        rejtveny[index, i] = temp[i];
+                    }
+                }
+
+                index++;
+            }
+
+            for (int i = 0; i < rejtveny.GetLength(0); i++)
+            {
+                for (int j = 0; j < rejtveny.GetLength(1); j++)
+                {
+                    if (rejtveny[i, j] == 'X')
+                    {
+                        Console.Write("█");
+                    }
+                    else
+                    {
+                        Console.Write(rejtveny[i, j]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Masodik()
+        {
+
+        }
+
+        static void Harmadik()
+        {
+
+        }
+
+        static void Negyedik()
+        {
+
+        }
+
+        static void Otodik()
+        {
+
+        }
+
+        static void Hatodik()
+        {
+
+        }
+
+        static void Hetedik()
+        {
+
+        }
+
+        static void Nyolcadik()
+        {
+
+        }
+    }
+}
